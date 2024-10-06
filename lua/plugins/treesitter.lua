676be80c106b62dd5d3b670dfd277a2db668ccf4
Andrii Stelmashenko <andrey.stelmashenko@gmail.com>
2024-10-06 14:58:39 +0300

    configured treesitter incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -7,11 +5,25 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    -- opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    --   "lua",
+    --   "go",
+    --   "rust"
+    --   -- add more arguments for adding more treesitter parsers
+    -- }),
+    vim.list_extend(opts.ensure_installed, {
       "lua",
       "go",
       "rust"
-      -- add more arguments for adding more treesitter parsers
     })
+    opts.incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<c-backspace>',
+        },
+    }
   end,
 }
