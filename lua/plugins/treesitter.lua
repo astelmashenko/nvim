add9647a4582b55caf53543ed2ab0acc6f6f8005
astelmashenko <andrey.stelmashenko@gmail.com>
2025-05-27 15:45:52 +0300

    qwe

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,29 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    -- opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    --   "lua",
+    --   "go",
+    --   "rust"
+    --   -- add more arguments for adding more treesitter parsers
+    -- }),
+    vim.list_extend(opts.ensure_installed, {
       "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+      "go",
+      "rust"
+    })
+    opts.incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<c-backspace>',
+        },
+    }
+  end,
 }
